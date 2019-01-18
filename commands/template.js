ec8124723d34a33e76018547d3dac46a0ec1b6ae
Jonas Alpha <jonasbyalpha@gmail.com>
2019-01-18 23:34:17 +0100

    TemplateJS

---
+++ commands/template.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run =async (bot, message, args) => {
+        message.channel.sendMessage('Code goes here');
+}
+
+
+
+module.exports.help = {
+    name: "template"
+}
