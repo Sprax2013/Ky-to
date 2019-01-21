214263d828ba1dc2fad59d22afed8629b456afdd
Jonas Alpha <jonasbyalpha@gmail.com>
2019-01-21 18:23:15 +0100

    lizzard is now fox

--- commands/fox.js
+++ commands/fox.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js'); // Praktisch für RichEmbed etc.
-const api = "https://nekos.life/api/lizard";
+const api = "https://nekos.life/api/v2/img/fox_girl";
 var imgNeko = ""
 const snekfetch = require('snekfetch');
 
 module.exports.cmd = {
-    name: 'lizard', // Führt onCommand aus, bei einer Nachricht wie '!template Arg1 Arg2'
+    name: 'fox', // Führt onCommand aus, bei einer Nachricht wie '!template Arg1 Arg2'
 };
 
 module.exports.onCommand = async (bot, msg, cmd, args) => {
      var imgNeko = snekfetch.get(api).then(r =>{
         const embed = new Discord.RichEmbed()
-                .setTitle("Oh look! Its the Zucc.")
+                .setTitle("Ring Ding Ding Ding Dinge Dinge Ding.")
                 .setColor(0x00AE86)
                 .setFooter(`${msg.author.tag}`+ `'s Waifu`)
                 .setImage(r.body.url)
