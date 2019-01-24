9ea4db984324503661fe595dd6a90d406b1a0cc9
Jonas Alpha <JonasbyAlpha@gmail.com>
2019-01-24 14:03:28 +0100

    read

---
+++ commands/google.js
@@ -0,0 +1,10 @@
+const index = require('./../index'); // Kann genutzt werden um z.B. die Prefix für eine Gilde zu ändern etc.
+const dc = require('discord.js'); // Praktisch für RichEmbed etc.+
+const google = require('google');
+module.exports.cmd = {
+    name: 'google', // Führt onCommand aus, bei einer Nachricht wie '!template Arg1 Arg2'
+};
+
+module.exports.onCommand = async (bot, message, msg, args, cmd) => {
+// let the Bot google stuff here :)
+}
---
+++ commands/invite.js
@@ -0,0 +1,16 @@
+const index = require('./../index'); // Kann genutzt werden um z.B. die Prefix für eine Gilde zu ändern etc.
+const dc = require('discord.js'); // Praktisch für RichEmbed etc.
+
+module.exports.cmd = {
+    name: 'invite', // Führt onCommand aus, bei einer Nachricht wie '!template Arg1 Arg2'
+};
+
+module.exports.onCommand = async (bot, msg, cmd, args) => {
+    const embed = new dc.RichEmbed()
+    .setTitle("Invite Link")
+    .setDescription(`Use **${index.getGuildPrefix(msg.guild.id)}commands** after the Bot is installed!`)
+    .setColor(0x00AE86)
+    .setURL(`https://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=526454942328946719`)
+    .setFooter(`${msg.author.username}`+ `'s request has been fulfilled`)
+    msg.channel.send(embed)
+}
